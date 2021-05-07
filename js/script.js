98a0738984959e52d4d111ae1ccb68fe515b9496
ira77kim <53447010+ira77kim@users.noreply.github.com>
2021-05-07 15:49:38 -0400

    d

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// let input = document.getElementById('write').value;
+
+
+// document.getElementById('cta1').onclick=get();
+let input
+function get(){
+    // input = document.getElementById('write').value;
+    window.location.href='believer.html';
+    console.log(input);
+}
+function last(){
+    window.location.href='after.html';
+}
+
+function mouseOver() {
+    input = document.getElementById('write').value;
+}
